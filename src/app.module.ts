dcba527738ece683ec009086adbb84df432e7dbc
RodriArrue <arruerodrigo28@gmail.com>
2026-06-16 14:54:25 -0300

    feat: project setup - NestJS + GraphQL + TypeORM + PubSub

---
+++ src/app.module.ts
@@ -0,0 +1,47 @@
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
+import { PubSub } from 'graphql-subscriptions';
+import { PUB_SUB } from './common/constants';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { PostsModule } from './posts/posts.module';
+import { CommentsModule } from './comments/comments.module';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      sortSchema: true,
+      subscriptions: {
+        'graphql-ws': true,
+      },
+      context: ({ req }: { req: Request }) => ({ req }),
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST || 'localhost',
+      port: parseInt(process.env.DB_PORT || '5432', 10),
+      username: process.env.DB_USERNAME || 'postgres',
+      password: process.env.DB_PASSWORD || 'postgres',
+      database: process.env.DB_NAME || 'graphql_blog',
+      autoLoadEntities: true,
+      synchronize: true, // Solo para desarrollo
+    }),
+    AuthModule,
+    UsersModule,
+    PostsModule,
+    CommentsModule,
+  ],
+  providers: [
+    {
+      provide: PUB_SUB,
+      useValue: new PubSub(),
+    },
+  ],
+  exports: [PUB_SUB],
+})
+export class AppModule {}
